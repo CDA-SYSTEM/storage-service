aa4d4af70b5bfdaa7438dd2294857d8470119d55
Ilesandres <ilesandres6@gmail.com>
2026-06-13 07:29:54 -0500

    chore: register FolderRepository in storage module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { cassandraProvider } from '../shared/database/cassandra.provider';
 import { StorageController } from './storage.controller';
 import { StorageRepository } from './repositories/storage.repository';
+import { FolderRepository } from './repositories/folder.repository';
 import { StorageService } from './storage.service';
 
 @Module({
   controllers: [StorageController],
-  providers: [cassandraProvider, StorageRepository, StorageService],
+  providers: [cassandraProvider, StorageRepository, FolderRepository, StorageService],
   exports: [StorageService],
 })
 export class StorageModule {}
